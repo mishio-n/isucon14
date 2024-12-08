e253aba0601ec4625923222e6bd8bb1fa9956220
yhcfu <yoshimune.hasegawa@gmail.com>
2024-12-08 13:00:03 +0900

    internalGetMatching のアルゴリズムを最も近い椅子選択に改善

--- webapp/go/internal_handlers.go
+++ webapp/go/internal_handlers.go
@@ -4,12 +4,17 @@ import (
 	"database/sql"
 	"errors"
 	"net/http"
+
+	"github.com/oklog/ulid/v2"
 )
 
-// このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
+/**
+ * [x] ランダムな椅子の選択　から 最も近い椅子を選択に変更した
+ * [ ] 椅子の状態をより詳細に管理し、例えば「移動中」「待機中」などの状態を追加する
+ * [ ] ライドリクエストの優先度を考慮し、例えばVIPユーザーのリクエストを優先するなどの方法があります
+ */
 func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	// MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
 	ride := &Ride{}
 	if err := db.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id IS NULL ORDER BY created_at LIMIT 1`); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
@@ -20,31 +25,29 @@ func internalGetMatching(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// 最も近い椅子を選択する
 	matched := &Chair{}
-	empty := false
-	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
-			if errors.Is(err, sql.ErrNoRows) {
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-			writeError(w, http.StatusInternalServerError, err)
-		}
-
-		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
-			writeError(w, http.StatusInternalServerError, err)
+	if err := db.GetContext(ctx, matched, `
+        SELECT *, 
+            (6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance 
+        FROM chairs 
+        WHERE is_active = TRUE 
+        ORDER BY distance 
+        LIMIT 1`, ride.PickupLatitude, ride.PickupLongitude, ride.PickupLatitude); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
-		if empty {
-			break
-		}
-	}
-	if !empty {
-		w.WriteHeader(http.StatusNoContent)
+		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
 
-	if _, err := db.ExecContext(ctx, "UPDATE rides SET chair_id = ? WHERE id = ?", matched.ID, ride.ID); err != nil {
+	if _, err := db.ExecContext(ctx, `UPDATE rides SET chair_id = ? WHERE id = ?`, matched.ID, ride.ID); err != nil {
+		writeError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if _, err := db.ExecContext(ctx, `INSERT INTO ride_statuses (id, ride_id, status) VALUES (?, ?, ?)`, ulid.Make().String(), ride.ID, "MATCHING"); err != nil {
 		writeError(w, http.StatusInternalServerError, err)
 		return
 	}
